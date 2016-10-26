310ada94de91364e092882014e21ab679a67f71e
annedien <annedient@gmail.com>
2016-10-26 10:09:59 +0200

    add order contentvblack by order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,8 @@ class PagesController < ApplicationController
   end
 
   def prijs
-    @content_block = ContentBlock.where(page: "Prijs en beschikbaarheid")
+    @content_block = ContentBlock.where(page: "Prijs en beschikbaarheid").order(:order)
+
   end
 
   def omgeving
