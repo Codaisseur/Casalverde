f40b6ada8398082a4e4d5939430c69604cede1b1
annedien <annedient@gmail.com>
2016-10-27 10:41:13 +0200

    add booking controller, seeds, bookingform

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   match '/contacts',     to: 'contacts#new',             via: 'get'
   resources "contacts", only: [:new, :create]
 
+  resources :bookings, only: [:create]
+
   get "faciliteiten" => "pages#faciliteiten"
   get "prijs" => "pages#prijs"
   get "omgeving" => "pages#omgeving"
   get "contact" => "pages#contact"
   get "home" => "pages#home"
+  get "booking" => "pages#booking"
 end
