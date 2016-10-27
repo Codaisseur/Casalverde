bc75445f607d623cd3b60b243452515f1f5a96c7
annedien <annedient@gmail.com>
2016-10-27 08:56:26 +0200

    add booking Model

---
+++ db/migrate/20161027065446_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :bookings do |t|
+      t.references :guestuser, foreign_key: true
+      t.datetime :starts_at
+      t.datetime :ends_at
+      t.decimal :price
+      t.decimal :total
+
+      t.timestamps
+    end
+  end
+end
